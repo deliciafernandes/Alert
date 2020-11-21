e4a69cc72e8fbc121a0601bec1925bb5bdfcc9e5
Delicia Fernandes <fernandesdelicia07@gmail.com>
2020-11-21 14:57:50 +0530

    Homepage almost done

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,3 @@
-import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,12 +5,27 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  Animation _animation;
   int _index = 1;
 
   // List<Widget> _widgetList = [
   //   WhatToDoBody(),
   // ];
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _animationController.repeat(reverse: true);
+    _animation = Tween(begin: 2.0, end: 15.0).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,37 +58,117 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      extendBody: true,
-      bottomNavigationBar: Container(
-        margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(40.0),
+      body: Padding(
+        padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Column(
+              children: [
+                FadeTransition(
+                  opacity: _animationController
+                      .drive(CurveTween(curve: Curves.easeOut)),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xffe8f4fa),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    ),
+                    child: Text(
+                      'Emergency Drill',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(15.0),
+                      padding: const EdgeInsets.all(3.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Text("My Awesome Border"),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(15.0),
+                      padding: const EdgeInsets.all(3.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Text("My Awesome Border"),
+                    )
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {
+                    //TODO: add camera functionality
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 130,
+                    margin: EdgeInsets.only(top: 30.0, bottom: 15.0),
+                    // child: Image.asset('assets/images/alert_icon.png'),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xffFCEDEE),
+                          blurRadius: _animation.value * 4.5,
+                          spreadRadius: _animation.value * 1.5,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text('Tap in case of an emergency',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black26, fontSize: 15)),
+                ),
+              ],
+            ),
+          ],
         ),
-        child: Material(
-          elevation: 8.0,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
-          child: BottomNavigationBar(
-            elevation: 0,
-            currentIndex: _index,
-            backgroundColor: Colors.transparent,
-            selectedItemColor: Color(0xffe45858),
-            iconSize: 20.0,
-            onTap: (int index) {
-              setState(() {
-                _index = index;
-              });
-            },
-            items: [
-              BottomNavigationBarItem(icon: Icon(Icons.add), label: "Feature"),
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.face),
-                  label: ""
-                      "Account"),
-            ],
-          ),
+      ),
+      bottomNavigationBar: bottomNavBar(),
+    );
+  }
+
+  Container bottomNavBar() {
+    return Container(
+      margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40.0),
+      ),
+      child: Material(
+        elevation: 3.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+        child: BottomNavigationBar(
+          elevation: 0,
+          currentIndex: _index,
+          backgroundColor: Colors.transparent,
+          selectedItemColor: Color(0xffe45858),
+          iconSize: 20.0,
+          onTap: (int index) {
+            setState(() {
+              _index = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: "Feature"),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.face),
+                label: ""
+                    "Account"),
+          ],
         ),
       ),
     );
