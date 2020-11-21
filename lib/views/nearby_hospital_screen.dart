3a819dc3d2ecb4ce854127b6f9e598c74f6c5eb4
Alan Dsilva <alandsilva2001@gmail.com>
2020-11-21 18:54:39 +0530

    Add location service

--- lib/views/nearby_hospital_screen.dart
+++ lib/views/nearby_hospital_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:location/location.dart';
 
 class NearbyHospitalScreen extends StatefulWidget {
   @override
@@ -6,8 +7,49 @@ class NearbyHospitalScreen extends StatefulWidget {
 }
 
 class _NearbyHospitalScreenState extends State<NearbyHospitalScreen> {
+  Location location = new Location();
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+
+  void initState() {
+    super.initState();
+    _locationConfig();
+  }
+
+  void _locationConfig() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _locationData == null
+        ? Center(
+            child: Text("Allow location permission to view"),
+          )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Display list of hospitals here
+            ],
+          );
   }
 }
