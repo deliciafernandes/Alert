f3125a340ef272dbf13b4fe34e1d0809b1c16bb8
Delicia Fernandes <fernandesdelicia07@gmail.com>
2020-11-21 12:14:56 +0530

    App bar done

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -8,6 +8,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Color(0xfffafafa),
+        brightness: Brightness.light,
+        automaticallyImplyLeading: false,
+        centerTitle: false,
+        leading: Icon(
+          Icons.location_on,
+          color: Colors.grey[500],
+        ),
+        titleSpacing: 0.0,
+        title: Text(
+          'location',
+          style: TextStyle(color: Colors.grey[500]),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.info_rounded,
+              size: 28,
+              color: Color(0xffe45858),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
